745b80ca0c2ad96bb0ea67c1608f557edd7eccb5
Ruofan Wu <592499239@qq.com>
2018-04-29 21:21:55 +0200

    .

---
+++ src/GUIMain.java
@@ -0,0 +1,3 @@
+public class GUIMain extends javafx.application.Application{
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-
+        Application.launch(GUIMain.class);
     }
 }
