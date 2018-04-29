703ccb55a36b5077b7a8a284ee56958713f8b7b6
Ruofan Wu <592499239@qq.com>
2018-04-29 21:26:25 +0200

    GUI

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -1,3 +1,27 @@
-public class GUIMain extends javafx.application.Application{
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.awt.*;
+
+public class GUIMain extends javafx.application.Application{
+    static Stage primaryStage;
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("RufuBot");
+
+        VBox root = new VBox(5.0);
+        root.setAlignment(Pos.CENTER);
+
+        root.setPadding(new Insets(20));
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        primaryStage.setX(screenSize.width / 2 - (600 / 2));
+        primaryStage.setY(screenSize.height / 2 - (400 / 2));
+
+        primaryStage.show();
+
+    }
 }
