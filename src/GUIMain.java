e5819f2ced476a29aedb69f52733178a4aa0b081
Ruofan Wu <592499239@qq.com>
2018-09-27 22:40:46 +0200

    Nightnight

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -1,14 +1,11 @@
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.awt.*;
-import java.util.Formatter;
+import java.util.Optional;
 
 public class GUIMain extends javafx.application.Application{
 
@@ -33,7 +30,38 @@ public class GUIMain extends javafx.application.Application{
         grid.setVgap(5);
         grid.setMinWidth(260);
 
+        Label profile = new Label("Profile");
+        //Print the lable
 
+        ChoiceBox<String> profiles = new ChoiceBox<>();
+        /*
+        profiles.getItems().add();
+         */
+
+
+        Button proceed = new Button("Proceed");
+
+        Button newProfile = new Button("New Profile");
+
+        Button deleteProfile = new Button("Delete Profile");
+
+        Button editProfile = new Button("Edit Profile");
+
+        Button apply = new Button("Apply");
+
+        apply.setOnAction(even -> {
+            Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+            confirm.setTitle("RufuBot");
+            confirm.setHeaderText(null);
+            confirm.setContentText("Sure you want to apply the current settings?");
+            Optional<ButtonType> answer = confirm.showAndWait();
+
+        });
+
+        Button cancel = new Button("Cancel");
+
+        ChoiceBox<String> action = new ChoiceBox<>();
+        action.getItems().addAll("Click", "Type", "Delay");
 
         return new Scene(root, 800, 600);
     }
