24fac3634068d276e5afa32bd5bcbd96290aa319
Ruofan Wu <592499239@qq.com>
2018-10-12 15:18:34 +0200

    .

--- src/GUI.java
+++ src/GUI.java
@@ -412,22 +412,15 @@ public class GUI extends javafx.application.Application{
 
                 @Override
                 public void mousePressed(MouseEvent e) {
-
                 }
-
                 @Override
                 public void mouseReleased(MouseEvent e) {
-
                 }
-
                 @Override
                 public void mouseEntered(MouseEvent e) {
-
                 }
-
                 @Override
                 public void mouseExited(MouseEvent e) {
-
                 }
             });
         });
